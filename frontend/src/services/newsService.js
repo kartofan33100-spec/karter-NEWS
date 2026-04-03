065dc9e6c0d82f9d847abd7904651571443141b9
kartofan33100-spec <kartofan33100@gmail.com>
2026-04-03 14:19:11 +0500

    Update newsService.js

--- frontend/src/services/newsService.js
+++ frontend/src/services/newsService.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api/articles';
+const API_URL = import.meta.env.VITE_API_URL;
 
 export async function getAllArticles(search = '', category = '') {
     const params = new URLSearchParams();
